e041bd772f90d221fdbc5d9d761ef55829fb5fd6
kevin puscan <kevinjpuscan@gmail.com>
2020-06-10 18:49:06 -0500

    deploy in github

--- src/config.js
+++ src/config.js
@@ -1,6 +1,9 @@
 const CONFIG = {
-  API_URL: process.env.REACT_APP_API_URL,
+  /*API_URL: process.env.REACT_APP_API_URL,
   URL: process.env.REACT_APP_URL,
-  SECRET_KEY: process.env.REACT_APP_SECRET_KEY
+  SECRET_KEY: process.env.REACT_APP_SECRET_KEY*/
+  API_URL: "http://157.245.138.14:8081",
+  URL: "https://kevinjpuscan.github.io/webrtc-multi-peer",
+  SECRET_KEY: "kevin-secret"
 };
 export default CONFIG;
