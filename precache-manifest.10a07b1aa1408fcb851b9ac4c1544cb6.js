a1393ae43e50c2d4650d37612f7626ff7eb7c8e5
kevin puscan <kevinjpuscan@gmail.com>
2020-06-10 19:09:31 -0500

    Updates

---
+++ precache-manifest.10a07b1aa1408fcb851b9ac4c1544cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a00f5bdc850d4f6e71c9cf1b2b7c5040",
+    "url": "/webrtc-multi-peer/index.html"
+  },
+  {
+    "revision": "c38fa2ca08cdb3ebcaed",
+    "url": "/webrtc-multi-peer/static/css/main.db561a72.chunk.css"
+  },
+  {
+    "revision": "8ca2790143d642693429",
+    "url": "/webrtc-multi-peer/static/js/2.766c095c.chunk.js"
+  },
+  {
+    "revision": "d3ff1a3b31b97563752a353cbeb5663f",
+    "url": "/webrtc-multi-peer/static/js/2.766c095c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c38fa2ca08cdb3ebcaed",
+    "url": "/webrtc-multi-peer/static/js/main.0ffeb782.chunk.js"
+  },
+  {
+    "revision": "2d7b9d842f9aa3e748d4",
+    "url": "/webrtc-multi-peer/static/js/runtime-main.aa271b25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.429379b3f59bbea3939e5837bb391007.js"
+  "/webrtc-multi-peer/precache-manifest.10a07b1aa1408fcb851b9ac4c1544cb6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/webrtc-multi-peer/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
